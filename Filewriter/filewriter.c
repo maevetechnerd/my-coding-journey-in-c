6c3fb25af7bd646016583a50881e8c2c43f1ed25
maevetechnerd <maefin@tutanota.com>
2023-07-14 17:04:04 +0200

    Did some changes to make it slightly more advanced, like adding a struct which allows for cleaner looking code, and added so you can enter age, height and a hobby too.

--- Filewriter/filewriter.c
+++ Filewriter/filewriter.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct person {
+    char name[25];
+    int age;
+    int height;
+    char hobby[25];
+};
+
 int main(int argc, char *argv[])
 {
-    char *character;
+    struct person person1;
+
+    person1.name;
+    person1.age;
+    person1.height;
+    person1.hobby;
 
     FILE *fp;
     fp = fopen("myfirstwrittenfile.bin", "a+");
-    
+
     if (fopen == NULL) {
         perror("Unable to write or open file");
     }
-    printf("Enter a character: ");
-    scanf("%c", &character);
-    fprintf(fp, "%c", character);
+
+    printf("Enter a name: ");
+    scanf("%25s", person1.name);
+    fprintf(fp, "%s:", person1.name);
+    printf("Enter age: ");
+    scanf("%d", &person1.age);
+    fprintf(fp, " %d,", person1.age);
+    printf("Enter height: ");
+    scanf("%d", &person1.height);
+    fprintf(fp, " %dcm,", person1.height);
+    printf("Enter a hobby: ");
+    scanf("%25s", person1.hobby);
+    fprintf(fp, " %s", person1.hobby);
+
+    return 0;
 }
