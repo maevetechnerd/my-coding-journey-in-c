054efbe829f98817d0e8748dfc250fc06c229086
maevetechnerd <maefin@tutanota.com>
2023-07-14 17:31:59 +0200

    Immidietly made some new changes again because I noticed that I didn't need to declare person1 name age and that stuff. I also added some comments to practice that alittle. Might see if I can add a function for the printing to clean the code up abit more.

--- Filewriter/filewriter.c
+++ Filewriter/filewriter.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* This is my first file writer I have made. It was quite easy. Though, there are ways to do improvements.
+ * This was something I thought would be cool to make and it was.
+ */
+
+
+/* This struct was is used for cleaner code. */
 struct person {
     char name[25];
     int age;
@@ -12,11 +18,6 @@ int main(int argc, char *argv[])
 {
     struct person person1;
 
-    person1.name;
-    person1.age;
-    person1.height;
-    person1.hobby;
-
     FILE *fp;
     fp = fopen("myfirstwrittenfile.bin", "a+");
 
@@ -24,18 +25,24 @@ int main(int argc, char *argv[])
         perror("Unable to write or open file");
     }
 
+    /* For manually putting in info to the file */
     printf("Enter a name: ");
     scanf("%25s", person1.name);
     fprintf(fp, "%s:", person1.name);
+
     printf("Enter age: ");
     scanf("%d", &person1.age);
     fprintf(fp, " %d,", person1.age);
+
     printf("Enter height: ");
     scanf("%d", &person1.height);
     fprintf(fp, " %dcm,", person1.height);
+
     printf("Enter a hobby: ");
     scanf("%25s", person1.hobby);
-    fprintf(fp, " %s", person1.hobby);
+    fprintf(fp, " %s\n", person1.hobby);
+
+    fclose(fp);
 
     return 0;
 }
